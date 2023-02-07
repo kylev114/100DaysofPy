10cc9c306486ebcc94c3619203abd0f364df7ee3
lolmanyaa <kylevo114@gmail.com>
2023-02-07 08:00:39 -0500

    updated comments

--- Day_05 - Password Generator.py
+++ Day_05 - Password Generator.py
@@ -11,19 +11,24 @@ numbers = []
 symbols = []
 password = []
 
+# Retrieve characters and seperates them into lists
 [letters.append(i) for i in string.ascii_letters]
 [numbers.append(i) for i in string.digits]
 [symbols.append(i) for i in string.punctuation]
 
 
 print("Welcome to the PyPassword Generator!")
+
+#Prompt user input for number of each character
 numLetters= int(input("How many letters would you like in your password?\n")) 
 numNumbers = int(input(f"How many numbers would you like?\n"))
 numSymbols = int(input(f"How many symbols would you like?\n"))
 
+# Add randomcharacter based on user input to password list
 [password.append(random.choice(letters)) for i in range(numLetters)]
 [password.append(random.choice(symbols)) for i in range(numSymbols)]
 [password.append(random.choice(numbers)) for i in range(numNumbers)]
 
+# Randomize password list Order
 random.shuffle(password)
 print(''.join(password))
